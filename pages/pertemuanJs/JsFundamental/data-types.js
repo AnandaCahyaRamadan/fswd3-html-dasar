5010ca255a14601b25afeac5b96965d2343f4fbc
AnandaCahyaRamadan <76020880+AnandaCahyaRamadan@users.noreply.github.com>
2023-03-15 09:02:14 +0700

    [FIX]

---
+++ pages/pertemuanJs/JsFundamental/data-types.js
@@ -0,0 +1,30 @@
+//Primitive
+let a = 25;
+console.log(a);
+console.log("------------------------------------")
+const bigInt = 111111117831839137131316531641541631341635163n;
+console.log(bigInt);
+console.log("------------------------------------");
+const nama = "rama";
+console.log(nama);
+console.log("------------------------------------");
+let greater = 10 > 5;
+console.log(greater);
+console.log("------------------------------------");
+let pacar = null;
+console.log(pacar);
+console.log("------------------------------------");
+let name;
+console.log(name);
+console.log("------------------------------------");
+let pegawai = Symbol ("Joko");
+console.log(pegawai);
+console.log(pegawai.description);
+console.log("------------------------------------");
+
+//Objek
+let mahasiswa = {
+    nama : "rama",
+    nim : "36205540101"
+}
+console.log(`Nama saya ${mahasiswa.nama} nim saya ${mahasiswa.nim}`);
